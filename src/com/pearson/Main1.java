881e78eaa687e4828612c677cfdb7b12d30c3cb5
anshu <anshuasthana2001@yhoo.com>
2025-02-15 17:54:30 -0600

    Change from IDE

--- src/com/pearson/Main1.java
+++ src/com/pearson/Main1.java
@@ -3,6 +3,6 @@ package com.pearson;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Second Program");
+        System.out.println("added new Second Program");
     }
-
 }
